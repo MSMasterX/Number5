a3acc633442ab8d84aabb2cba5a4a4b645678fb9
( Odi ) Thanapat Sombatkamrai <30354428+MSMasterX@users.noreply.github.com>
2022-06-07 13:02:27 +0200

    Hello + input from user

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Whats your name:");
+        String name= input.next();
+        System.out.println("Hello \n"+name);
+    }
+}
